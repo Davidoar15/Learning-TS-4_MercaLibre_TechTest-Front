ddca9b3c4677a282222049083f03877c0c165920
Davidoar15 <davidoar15@gmail.com>
2023-09-01 19:36:05 -0300

    add/loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import type { Metadata } from "next";
 
@@ -15,9 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header style={{height: '3rem', backgroundColor: 'yellow', padding: "0 4rem", display: "flex"}}>
-          <form action='/items' style={{margin: 'auto', display: 'flex', flex: "1", gap: '16px'}}>
+          <form action='/items' style={{margin: 'auto', display: 'flex', flex: "1", gap: '16px', alignItems: 'center'}}>
+            <Link href={'/'} style={{textDecoration: 'none', color: "black", backgroundColor: 'white', border: "2px blue solid", borderRadius: '10px', padding: "4px"}}>
+              Merca Libre
+            </Link>
             <input style={{height: '1.3rem', flex: '1', paddingLeft: "0.5rem", paddingRight: "0.5rem"}} type="text" name="search" placeholder="Search Product"/>
-            <button style={{backgroundColor: "lightblue", cursor: 'pointer'}} type="submit">
+            <button style={{backgroundColor: "lightblue", cursor: 'pointer', padding: '4px'}} type="submit">
               Search
             </button>
           </form>
